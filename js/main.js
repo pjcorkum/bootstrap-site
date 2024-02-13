fd4fc3230c74052fa80698ada9837cd328f2ff16
PJ Corkum <pjcorkum@metacoastal.com>
2024-02-13 14:36:13 -0500

    Fixed everything

--- js/main.js
+++ js/main.js
@@ -5,9 +5,19 @@
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
-//
-// Place any custom JS here
-//
+document.getElementById('ultimateFrisbeeButton').addEventListener('click', getRolled);
+
+function getRolled() {
+  // Change the variables below to your liking
+  const currentURL = "https://shattereddisk.github.io/rickroll/rickroll.mp4";
+  const pageTitle = "Loading...";
+  // End of changable variables
+
+    document.title = pageTitle;
+
+    window.location.href = currentURL;
+    console.log("test");
+}
 
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
